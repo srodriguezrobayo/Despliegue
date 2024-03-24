29d118f2be1ddfe19568351712e713c7b4943528
srodriguezrobayo <113036583+srodriguezrobayo@users.noreply.github.com>
2024-03-23 19:54:00 -0500

    conexion

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://d10hebfxsclsy2.cloudfront.net');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
